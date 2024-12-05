d42f5b6aa29401c4d23bf2d4c72a0b88110dd5cb
Tubby <git@tubby.nl>
2024-12-05 14:37:58 +0100

    as-is refactor on JavaOpenBankingCodegen

--- open-banking-driver-generator/src/main/java/com/ing/developer/codegen/JavaOpenBankingCodegen.java
+++ open-banking-driver-generator/src/main/java/com/ing/developer/codegen/JavaOpenBankingCodegen.java
@@ -6,15 +6,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.parameters.Parameter;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+
+import java.util.*;
 import org.openapitools.codegen.languages.JavaClientCodegen;
 
 public class JavaOpenBankingCodegen extends JavaClientCodegen {
-
-  private final List<String> excludedParams = Arrays.asList("Signature", "Digest", "Date");
+  private final static List<String> EXCLUDED_PARAMS = List.of("Signature", "Digest", "Date");
+  private final static List<String> NON_REQUIRED_PARAMS = List.of("Authorization", "X-JWS-Signature");
 
   @Override
   public String getName() {
@@ -28,22 +26,30 @@ public class JavaOpenBankingCodegen extends JavaClientCodegen {
 
   @Override
   public void preprocessOpenAPI(OpenAPI openAPI) {
-    if (openAPI != null && openAPI.getPaths() != null) {
-      for (Map.Entry<String, PathItem> entry : openAPI.getPaths().entrySet()) {
-        for (Operation operation : entry.getValue().readOperations()) {
-          if (operation.getParameters() != null) {
-            List<Parameter> includedParams = operation.getParameters().stream()
-                .filter(not(p -> excludedParams.contains(p.getName())))
-                .collect(Collectors.toList());
-            includedParams.stream().filter(p -> "Authorization".equals(p.getName()))
-                .forEach(p -> p.required(false));
-            includedParams.stream().filter(p -> "X-JWS-Signature".equals(p.getName()))
-                .forEach(p -> p.required(false));
-            operation.setParameters(includedParams);
-          }
-        }
-      }
-    }
+    Optional.ofNullable(openAPI)
+            .stream()
+            .map(OpenAPI::getPaths)
+            .map(Map::values)
+            .flatMap(Collection::stream)
+            .map(PathItem::readOperations)
+            .flatMap(Collection::stream)
+            .forEach(JavaOpenBankingCodegen::updateOperationParameters);
     super.preprocessOpenAPI(openAPI);
+  }
+
+  private static void updateOperationParameters(Operation operation) {
+    Optional.ofNullable(operation.getParameters())
+            .map(JavaOpenBankingCodegen::updateParameters)
+            .ifPresent(operation::setParameters);
+  }
+
+  private static List<Parameter> updateParameters(List<Parameter> input) {
+    List<Parameter> includedParams = input.stream()
+            .filter(not(p -> EXCLUDED_PARAMS.contains(p.getName())))
+            .toList();
+    includedParams.stream()
+            .filter(p -> NON_REQUIRED_PARAMS.contains(p.getName()))
+            .forEach(p -> p.required(false));
+    return includedParams;
   }
 }
